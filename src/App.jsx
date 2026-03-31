a51545eb03ecf61e4b401d4eba1bd554375adcd3
mutumaz <mutumabizz@gmail.com>
2026-03-31 10:11:08 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Routes, Route } from "react-router-dom";
+import Home from './Pages/Home'
+import Booking from './Pages/Booking'
+import Bookings from './Pages/Bookings'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  return (
+
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/booking/:id" element={<Booking />} />
+        <Route path="/bookings" element={<Bookings />} />
+      </Routes>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Data/Dentists.js
@@ -0,0 +1,4 @@
+export const dentists = [
+    { id: 1, name: "Clara Collins" },
+    { id: 2, name: "Charles Gates" }
+]
