131c1ac82ebb3ad292a8c949fff3ed9c5b95a2e9
mutumaz <mutumabizz@gmail.com>
2026-04-01 09:13:20 +0300

    add logic for booking page

---
+++ src/Pages/Booking.jsx
@@ -0,0 +1,16 @@
+import { useParams } from "react-router-dom";
+import BookingForm from '../Components/BookingForm/BookingForm'
+
+const Booking = () => {
+    const { id } = useParams()
+
+    return (
+        <div>
+            <h2>Booking Dentist ID:{id}</h2>
+            <BookingForm dentistId={id} />
+        </div>
+    )
+}
+
+
+export default Booking
