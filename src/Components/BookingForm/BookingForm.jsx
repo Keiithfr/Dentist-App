ed52b277871a6ce68f08128861be599d17b13fe6
mutumaz <mutumabizz@gmail.com>
2026-04-02 09:49:34 +0300

    update BookingForm with main logic

---
+++ src/Components/BookingForm/BookingForm.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+const BookingForm = () => {
+    const [form, setForm] = useState({
+        name: '',
+        date: '',
+        time: ''
+    })
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!form.name || !form.time || form.date) {
+            alert('fill all the fields')
+            return
+        }
+
+        const bookings = JSON.parse(localStorage.getItem('bookings')) || []
+
+        const exists = bookings.find(
+            (b) => b.date === form.date && b.time === form.time
+        )
+
+        if (exists) {
+            alert('Time already booked')
+            return
+        }
+
+        const newBooking = {
+            ...form, dentistId, id: Date.now
+        }
+
+        bookings.push(newBooking)
+        localStorage.setItem('bookings', JSON.stringify(bookings))
+        alert('Booked!')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input name="name" placeholder="Name" onChange={handleChange} />
+            <input type="date" name="date" onChange={handleChange} />
+            <input type="time" name="time" onChange={handleChange} />
+
+            <button type="submit">Book</button>
+        </form>
+    )
+}
+
+export default BookingForm
