befb723a0f567454116c37bb5078730e62888e56
mutumaz <mutumabizz@gmail.com>
2026-05-04 09:32:04 +0300

    update private route

--- src/Components/PrivateRoute.jsx
+++ src/Components/PrivateRoute.jsx
@@ -3,7 +3,11 @@ import { Navigate } from "react-router-dom";
 const PrivateRoute = ({ children }) => {
     const token = localStorage.getItem("token");
 
-    return token ? children : <Navigate to="/login" />;
+    if (!token || token === "undefined" || token === "null") {
+        return <Navigate to="/login" />
+    }
+
+    return children;
 };
 
 export default PrivateRoute;
